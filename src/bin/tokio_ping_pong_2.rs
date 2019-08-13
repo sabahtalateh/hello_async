cce1ee60e10d78e893ad4168e3797cac1557321c
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2019-08-13 14:20:16 +0300

    Ping Pong Async

--- src/bin/tokio_ping_pong_2.rs
+++ src/bin/tokio_ping_pong_2.rs
@@ -16,38 +16,39 @@ use std::time::{Duration, Instant};
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
 use tokio::timer::Delay;
+use tokio::sync::oneshot::{Sender, Receiver};
 
-const TRANSPORT_MAX_DELAY: Duration = Duration::from_secs(1);
+const TRANSPORT_MAX_DELAY: Duration = Duration::from_secs(0);
+const TRANSPORT_MIN_DELAY: Duration = Duration::from_secs(10);
 
-type Message = oneshot::Sender<Duration>;
+type Message = (usize, oneshot::Sender<Duration>);
 
 struct Transport {
+    min_delay: Duration,
     max_delay: Duration,
 }
 
 impl Transport {
-    fn new(max_delay: Duration) -> Transport {
-        Transport { max_delay }
+    fn new(min_delay: Duration, max_delay: Duration) -> Transport {
+        Transport { min_delay, max_delay }
     }
 
     fn default() -> Transport {
-        Transport {
-            max_delay: TRANSPORT_MAX_DELAY,
-        }
+        Transport::new(TRANSPORT_MAX_DELAY, TRANSPORT_MIN_DELAY)
     }
 
-    fn send_ping(&self) {
-        println!("entering ping");
+    fn send_ping(&self, thread_id: &usize) {
+        println!("[{}] entering ping", thread_id);
     }
 
-    fn recv_pong(&self) -> impl Future<Item = (), Error = io::Error> {
-        println!("entering recv_pong");
+    fn recv_pong(&self, thread_id: &usize) -> impl Future<Item = (), Error = io::Error> {
+        print!("[{}] entering recv_pong", thread_id);
 
         let mut rng = rand::thread_rng();
-        let wait_millis: u64 = rng.gen_range(0, self.max_delay.as_millis()) as u64;
+        let wait_millis: u64 = rng.gen_range(self.min_delay.as_millis(), self.max_delay.as_millis()) as u64;
         let delay = Duration::from_millis(wait_millis);
 
-        println!("Delay {:?}", delay);
+        println!(" delay {:?}", delay);
 
         let when = Instant::now() + delay;
 
@@ -60,23 +61,24 @@ impl Transport {
 fn coordinator_task(rx: mpsc::Receiver<Message>) -> impl Future<Item = (), Error = ()> {
     let transport = Transport::default();
 
-    rx.map_err(|_| ()).for_each(move |r_tx| {
+    rx.map_err(|_| ()).for_each(move |(thread_id, r_tx)| {
         let start = Instant::now();
 
-        transport.send_ping();
+        transport.send_ping(&thread_id);
 
-        transport.recv_pong().map_err(|_| ()).and_then(move |_| {
+        let fut = transport.recv_pong(&thread_id).map_err(|_| ()).and_then(move |_| {
             let rtt = start.elapsed();
             r_tx.send(rtt).unwrap();
             Ok(())
-        })
+        });
+        tokio::spawn(fut)
     })
 }
 
-fn rtt(tx: mpsc::Sender<Message>) -> impl Future<Item = (Duration, mpsc::Sender<Message>), Error = ()> {
-    let (r_tx, r_rx) = oneshot::channel();
+fn rtt(thread_id: &usize, tx: mpsc::Sender<Message>) -> impl Future<Item = (Duration, mpsc::Sender<Message>), Error = ()> {
+    let (r_tx, r_rx) : (Sender<Duration>, Receiver<Duration>) = oneshot::channel();
 
-    tx.send(r_tx)
+    tx.send((*thread_id, r_tx))
         .map_err(|_| ())
         .and_then(|tx| {
             r_rx
@@ -95,12 +97,12 @@ fn main() {
         tokio::spawn(coordinator_task(rx));
 
         // Spawn a few tasks that use the coordinator to requst RTTs.
-        for _ in 0..4 {
+        for id in 0..10 {
             let tx = tx.clone();
 
-            tokio::spawn(lazy(|| {
-                rtt(tx).and_then(|(d, _)| {
-                    println!("{:?}", d);
+            tokio::spawn(lazy(move || {
+                rtt(&id, tx).and_then(move |(d, _)| {
+                    println!("[{}] >>> rtt = {:?}", id, d);
                     Ok(())
                 })
             }));
